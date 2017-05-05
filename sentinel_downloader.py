e55c4360226cabef1df914230c631c81d3da2f5e
Hannah Augustin <hannah.augustin@gmail.com>
2017-05-05 21:26:14 +0200

    UI exclusive option logic, tile search

--- sentinel_downloader.py
+++ sentinel_downloader.py
@@ -20,13 +20,15 @@
  *                                                                         *
  ***************************************************************************/
 """
-from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
-from PyQt4.QtGui import QAction, QIcon
+from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication, QDate
+from PyQt4.QtGui import QAction, QIcon, QMessageBox
 # Initialize Qt resources from file resources.py
 import resources
 # Import the code for the dialog
 from sentinel_downloader_dialog import SentinelDownloaderDialog
 import os.path
+import requests
+import ast
 
 
 class SentinelDownloader:
@@ -61,10 +63,16 @@ class SentinelDownloader:
 
         # Declare instance attributes
         self.actions = []
+
+        #
+        # Set plugin label under 'Web' tab in QGIS toolbar.
+        #
         self.menu = self.tr(u'&Sentinel Downloader')
+
         # TODO: We are going to let the user set this up in a future iteration
         self.toolbar = self.iface.addToolBar(u'SentinelDownloader')
         self.toolbar.setObjectName(u'SentinelDownloader')
+
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
@@ -184,10 +192,175 @@ class SentinelDownloader:
         """Run method that performs all the real work"""
         # show the dialog
         self.dlg.show()
-        # Run the dialog event loop
-        result = self.dlg.exec_()
-        # See if OK was pressed
-        if result:
-            # Do something useful here - delete the line containing pass and
-            # substitute with your code.
-            pass
+
+        self.dlg.btnSearch.clicked.connect(self.get_arguments)
+        self.dlg.btnTileSearch.clicked.connect(self.get_tile_coords)
+
+        # # Run the dialog event loop
+        # result = self.dlg.exec_()
+        # # See if OK was pressed
+        # if result:
+        #     # Do something useful here - delete the line containing pass and
+        #     # substitute with your code.
+        #     pass
+
+    def get_arguments(self):
+
+        #
+        # Create options namespace. Perhaps (definitely) bad practice, but whatever.
+        #
+        options = Namespace()
+
+        options.user = self.dlg.user_lineEdit.text()
+        options.password = self.dlg.pass_lineEdit.text()
+
+        if self.dlg.hub_comboBox.currentText() == 'API Hub':
+            options.hub = 'apihub'
+        elif self.dlg.hub_comboBox.currentText() == 'Dhus':
+            options.hub = 'dhus'
+
+        if self.dlg.sensor_comboBox.currentText() == 'All':
+            options.sentinel = 'all'
+        elif self.dlg.sensor_comboBox.currentText() == 'Sentinel-1':
+            options.sentinel = 'S1'
+        elif self.dlg.sensor_comboBox.currentText() == 'Sentinel-1A':
+            options.sentinel = 'S1A'
+        elif self.dlg.sensor_comboBox.currentText() == 'Sentinel-1B':
+            options.sentinel = 'S1B'
+        elif self.dlg.sensor_comboBox.currentText() == 'Sentinel-2':
+            options.sentinel = 'S2'
+        elif self.dlg.sensor_comboBox.currentText() == 'Sentinel-2A':
+            options.sentinel = 'S2A'
+        elif self.dlg.sensor_comboBox.currentText() == 'Sentinel-2B':
+            options.sentinel = 'S2B'
+
+        options.sortby = self.dlg.sortBy_comboBox.currentText()
+        options.lonmin = self.dlg.LLX_lineEdit.text()
+        options.lonmax = self.dlg.ULX_lineEdit.text()
+        options.latmin = self.dlg.LLY_lineEdit.text()
+        options.lonmax = self.dlg.ULY_lineEdit.text()
+        options.tile = self.dlg.s2Tile_lineEdit.text()
+        options.writedir = self.dlg.writeDir_txtPath.text()
+        options.s1mode = self.dlg.s1Mode_comboBox.currentText()
+        options.s1polar = self.dlg.s1Polar_comboBox.currentText()
+        options.s1product = self.dlg.s1Product_comboBox.currentText()
+        if self.dlg.cloudCover_enable.isChecked():
+            options.max_cloud = self.dlg.cloudCover_spinBox.cleanText()
+        options.absorbit = self.dlg.absOrbit_lineEdit.text()
+        options.orbitdir = self.dlg.orbitDir_comboBox.currentText()
+        options.relorbit = self.dlg.relOrbit_lineEdit.text()
+
+        if self.dlg.ingest_enable.isChecked():
+
+            #
+            # Get ingestion dates.
+            #
+            options.start_ingest_date = self.dlg.ingestFrom_dateEdit.date()
+            options.end_ingest_date = self.dlg.ingestTo_dateEdit.date()
+
+            #
+            # Convert ingestion dates to proper ISO format.
+            #
+            options.start_ingest_date = QDate.toString(
+                options.start_ingest_date, 'yyyy-MM-dd')
+            options.end_ingest_date = QDate.toString(
+                options.end_ingest_date, 'yyyy-MM-dd')
+
+        if self.dlg.date_enable.isChecked():
+
+            #
+            # Get sensing dates in date format.
+            #
+            options.start_date = self.dlg.dateFrom_dateEdit.date()
+            options.end_date = self.dlg.dateTo_dateEdit.date()
+
+            #
+            # Convert sensing dates to proper ISO format.
+            #
+            options.start_date = QDate.toString(options.start_date, 'yyyy-MM-dd')
+            options.end_date = QDate.toString(options.end_date, 'yyyy-MM-dd')
+
+        options.MaxRecords = self.dlg.maxRecords_spinBox.cleanText()
+
+
+        #
+        # Print arguments to message box for test.
+        #
+        self.text_to_messagebox(options)
+
+
+    def text_to_messagebox(self, options):
+
+        options_dict = vars(options)
+        options_string = ''
+        for key,value in options_dict.iteritems():
+            options_string += '{} : {}\n'.format(key,value)
+
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("Check the detailed text to see the options!")
+        msg.setInformativeText("This is additional information")
+        msg.setWindowTitle("MessageBox demo")
+        msg.setDetailedText(options_string)
+        msg.exec_()
+
+    def get_tile_coords(self):
+
+        s2_tile = self.dlg.s2Tile_lineEdit.text()
+        coords = self.kml_api(s2_tile)
+        try:
+            lon = str(round((float(coords[0])), 3))
+            lat = str(round((float(coords[1])), 3))
+            coords_str = '{}: lon {}, lat {}'.format(s2_tile, lon, lat)
+            self.dlg.tileSearch_textBrowser.setText(coords_str)
+        except:
+            self.dlg.tileSearch_textBrowser.setText('API failed or tile not found.')
+
+
+    def kml_api(self, tile):
+
+        '''This function returns the center point of a defined S2 tile based on an
+            API developed by M. Sudmanns, or from the kml file if request fails.'''
+
+        #
+        # Formulate request and get it.
+        #
+        with requests.Session() as s:
+
+            api_request = ('http://cf000008.geo.sbg.ac.at/cgi-bin/s2-dashboard/'
+                'api.py?centroid={}').format(tile)
+
+            try:
+                r = s.get(api_request)
+
+                #
+                # Read string result as a dictionary.
+                #
+                result = {}
+                result = r.text
+                result = ast.literal_eval(result)
+
+            #
+            # Catch base-class exception.
+            #
+            except requests.exceptions.RequestException as e:
+                #print '\n\n{}\n\n'.format(e)
+                result = {"status": "FAIL"}
+
+        #
+        # Extract lat, lon from API request, or try to get from file if failed.
+        #
+        if result["status"] == "OK" and result["data"]:
+            coords = [result["data"]["x"], result["data"]["y"]]
+
+        else:
+            return 'API failed.'
+
+        return coords
+
+#
+# Ultimately this exists to create an empty namespace similar to the output of
+# optparse or argparse to keep from having to change much code from before.
+#
+class Namespace(object):
+    pass
--- sentinel_downloader_dialog.py
+++ sentinel_downloader_dialog.py
@@ -23,7 +23,7 @@
 
 import os
 
-from PyQt4 import QtGui, uic
+from PyQt4 import QtGui, QtCore, uic
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'sentinel_downloader_dialog_base.ui'))
@@ -39,3 +39,220 @@ class SentinelDownloaderDialog(QtGui.QDialog, FORM_CLASS):
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
+
+        #
+        # All fields, buttons, etc. for reference.
+        #
+        # self.btnClose
+        # self.btnSearch
+        # self.btnDownload
+        # self.btnClear
+        # self.pass_lineEdit
+        # self.user_lineEdit
+        # self.hub_comboBox
+        # self.sensor_comboBox
+        # self.sortBy_comboBox
+        # self.LLX_lineEdit
+        # self.ULX_lineEdit
+        # self.LLY_lineEdit
+        # self.ULY_lineEdit
+        # self.s2Tile_lineEdit
+        # self.writeDir_toolButton
+        # self.writeDir_txtPath
+        # self.s1Mode_comboBox
+        # self.s1Polar_comboBox
+        # self.s1Product_comboBox
+        # self.cloudCover_enable
+        # self.cloudCover_spinBox
+        # self.absOrbit_lineEdit
+        # self.orbitDir_comboBox
+        # self.relOrbit_lineEdit
+        # self.ingestFrom_dateEdit
+        # self.ingestTo_dateEdit
+        # self.ingest_enable
+        # self.dateFrom_dateEdit
+        # self.dateTo_dateEdit
+        # self.date_enable
+        # self.maxRecords_spinBox
+        # self.results_tableWidget
+        # self.btnTileSearch
+        # self.tileSearch_textBrowser
+
+        #
+        # Hide password.
+        #
+        self.pass_lineEdit.setEchoMode(QtGui.QLineEdit.Password)
+
+        #
+        # Depending on platform chosen, enable or disable options.
+        #
+        self.sensor_comboBox.currentIndexChanged.connect(self.enable_platform)
+
+        #
+        # Disable tile query button and results window until tile entered.
+        #
+        self.btnTileSearch.setEnabled(False)
+        self.tileSearch_textBrowser.setEnabled(False)
+        #
+        # Enable tile center coordinate search button.
+        #
+        self.s2Tile_lineEdit.textChanged.connect(self.enable_tile_search)
+
+        #
+        # Add directory search functionality.
+        #
+        # self.writeDir_toolButton
+
+        #
+        # Disable date, ingestion date and cloudcover options until enabled.
+        #
+        self.dateFrom_dateEdit.setEnabled(False)
+        self.dateTo_dateEdit.setEnabled(False)
+        self.dateFrom_label.setEnabled(False)
+        self.dateTo_label.setEnabled(False)
+        self.ingestFrom_dateEdit.setEnabled(False)
+        self.ingestTo_dateEdit.setEnabled(False)
+        self.ingestFrom_label.setEnabled(False)
+        self.ingestTo_label.setEnabled(False)
+        self.cloudCover_spinBox.setEnabled(False)
+        self.cloudCover_label.setEnabled(False)
+
+        #
+        # If enable checkbox clicked, enable options, if not, disable.
+        #
+        self.date_enable.stateChanged.connect(self.enable_date)
+        self.ingest_enable.stateChanged.connect(self.enable_ingest)
+        self.cloudCover_enable.stateChanged.connect(self.enable_cloudcover)
+
+        #
+        # Set "to" dates to current day.
+        #
+        today = QtCore.QDate.currentDate()
+        self.ingestTo_dateEdit.setDate(today)
+        self.dateTo_dateEdit.setDate(today)
+
+
+
+    def enable_cloudcover(self):
+
+        '''This function toggles the max cloud cover input field and label
+            depending on whether the 'enable' checkbox has been checked.'''
+
+        if self.cloudCover_enable.isChecked() == True:
+            self.cloudCover_spinBox.setEnabled(True)
+            self.cloudCover_label.setEnabled(True)
+
+        else:
+            self.cloudCover_spinBox.setEnabled(False)
+            self.cloudCover_label.setEnabled(False)
+
+
+    def enable_date(self):
+
+        '''This function toggles sensing date input fields and labels
+            depending on whether the 'enable' checkbox has been checked.'''
+
+        if self.date_enable.isChecked() == True:
+            self.dateFrom_dateEdit.setEnabled(True)
+            self.dateTo_dateEdit.setEnabled(True)
+            self.dateFrom_label.setEnabled(True)
+            self.dateTo_label.setEnabled(True)
+
+        else:
+            self.dateFrom_dateEdit.setEnabled(False)
+            self.dateTo_dateEdit.setEnabled(False)
+            self.dateFrom_label.setEnabled(False)
+            self.dateTo_label.setEnabled(False)
+
+
+    def enable_ingest(self):
+
+        '''This function toggles ingestion date input fields and labels
+            depending on whether the 'enable' checkbox has been checked.'''
+
+        if self.ingest_enable.isChecked() == True:
+            self.ingestFrom_dateEdit.setEnabled(True)
+            self.ingestTo_dateEdit.setEnabled(True)
+            self.ingestFrom_label.setEnabled(True)
+            self.ingestTo_label.setEnabled(True)
+
+        else:
+            self.ingestFrom_dateEdit.setEnabled(False)
+            self.ingestTo_dateEdit.setEnabled(False)
+            self.ingestFrom_label.setEnabled(False)
+            self.ingestTo_label.setEnabled(False)
+
+    def enable_platform(self):
+
+        if (self.sensor_comboBox.currentText() == 'Sentinel-2'
+                or self.sensor_comboBox.currentText() == 'Sentinel-2A'
+                or self.sensor_comboBox.currentText() == 'Sentinel-2B'):
+
+            self.s2Tile_label.setEnabled(True)
+            self.s2Tile_lineEdit.setEnabled(True)
+            self.btnTileSearch.setEnabled(True)
+            self.tileSearch_textBrowser.setEnabled(True)
+
+            self.s1Mode_label.setEnabled(False)
+            self.s1Mode_comboBox.setEnabled(False)
+            self.s1Polar_label.setEnabled(False)
+            self.s1Polar_comboBox.setEnabled(False)
+            self.s1Product_label.setEnabled(False)
+            self.s1Product_comboBox.setEnabled(False)
+
+            self.cloudCover_enable.setEnabled(True)
+            if self.cloudCover_enable.isChecked() == True:
+                self.cloudCover_spinBox.setEnabled(True)
+                self.cloudCover_label.setEnabled(True)
+
+
+        elif (self.sensor_comboBox.currentText() == 'Sentinel-1'
+                or self.sensor_comboBox.currentText() == 'Sentinel-1A'
+                or self.sensor_comboBox.currentText() == 'Sentinel-1B'):
+
+            self.s2Tile_label.setEnabled(False)
+            self.s2Tile_lineEdit.setEnabled(False)
+            self.btnTileSearch.setEnabled(False)
+            self.tileSearch_textBrowser.setEnabled(False)
+
+            self.s1Mode_label.setEnabled(True)
+            self.s1Mode_comboBox.setEnabled(True)
+            self.s1Polar_label.setEnabled(True)
+            self.s1Polar_comboBox.setEnabled(True)
+            self.s1Product_label.setEnabled(True)
+            self.s1Product_comboBox.setEnabled(True)
+
+            self.cloudCover_enable.setEnabled(False)
+            self.cloudCover_spinBox.setEnabled(False)
+            self.cloudCover_label.setEnabled(False)
+            self.cloudCover_enable.setChecked(False)
+
+        else:
+
+            self.s2Tile_label.setEnabled(False)
+            self.s2Tile_lineEdit.setEnabled(False)
+            self.btnTileSearch.setEnabled(False)
+            self.tileSearch_textBrowser.setEnabled(False)
+
+            self.s1Mode_label.setEnabled(True)
+            self.s1Mode_comboBox.setEnabled(True)
+            self.s1Polar_label.setEnabled(True)
+            self.s1Polar_comboBox.setEnabled(True)
+            self.s1Product_label.setEnabled(True)
+            self.s1Product_comboBox.setEnabled(True)
+
+            self.cloudCover_enable.setEnabled(True)
+            if self.cloudCover_enable.isChecked() == True:
+                self.cloudCover_spinBox.setEnabled(True)
+                self.cloudCover_label.setEnabled(True)
+
+
+    def enable_tile_search(self):
+
+        if str(len(self.s2Tile_lineEdit.text())) == '5':
+            self.btnTileSearch.setEnabled(True)
+            self.tileSearch_textBrowser.setEnabled(True)
+        else:
+            self.btnTileSearch.setEnabled(False)
+            self.tileSearch_textBrowser.setEnabled(False)
+            self.tileSearch_textBrowser.setText('')
